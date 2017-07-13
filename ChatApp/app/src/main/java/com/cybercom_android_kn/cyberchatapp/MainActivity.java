5b6151e6302dad090c23a03a57448d0ec1c704c3
Saqib Sarker <saqib.sarker@gmail.com>
2017-07-13 17:10:55 +0200

    GoogleSignInButton
    Working googleSignInButton

--- ChatApp/app/src/main/java/com/cybercom_android_kn/cyberchatapp/MainActivity.java
+++ ChatApp/app/src/main/java/com/cybercom_android_kn/cyberchatapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -16,13 +17,23 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener{
 
-    private final String TAG = this.getClass().getName();
+    private final String TAG = "MainActivity";
     private static final int RC_SIGN_IN = 9001;
 
     private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth mAuth;
     private TextView textViewName;
 
     @Override
@@ -30,72 +41,70 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewName = (TextView) findViewById(R.id.textView_name);
+        textViewName = (TextView) findViewById(R.id.textView_userName);
+        textViewName.setText("In onCreate()");
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        // [START config_signin]
+        // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
+        // [END config_signin]
 
-        // Build a GoogleApiClient with access to the Google Sign-In API and the
-        // options specified by gso.
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
+
+        Log.d(TAG, "end of onCreate");
     }
 
     @Override
-    protected void onStart() {
+    public void onStart() {
         super.onStart();
-
-        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
-
-        if(opr.isDone())
-        {
-            Log.d(TAG, "Got cached sign-in");
-        }
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    public void onButtonClick(View view) {
-        switch (view.getId())
-        {
-            case R.id.sign_in_button_google:
-                signIn();
-                break;
-        }
     }
 
     private void signIn() {
+        textViewName.setText("In signIn()");
+        Log.d(TAG, "signIn()");
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
+    // [START onactivityresult]
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == RC_SIGN_IN)
-        {
+        if (requestCode == RC_SIGN_IN){
+            Log.d(TAG, "onActivityResult() in side if");
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleSignInResult(result);
         }
-    }
+    }// [END onactivityresult]
 
     private void handleSignInResult(GoogleSignInResult result) {
-        Log.d(TAG, "handleSignInResult" + result.isSuccess());
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        GoogleSignInAccount acct = result.getSignInAccount();
+        textViewName.setText("Hello" + acct.getDisplayName());
+    }
 
-        if (result.isSuccess())
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+
+    public void onClick(View view) {
+        Log.d(TAG, "onClick");
+        switch (view.getId())
         {
-            //Sign in Okey
-            GoogleSignInAccount acct = result.getSignInAccount();
-            textViewName.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            case R.id.sign_in_button:
+                signIn();
+                break;
         }
     }
 }
